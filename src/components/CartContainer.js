a3314869895b651849e4d9bb703329c7bd65c5ae
Nina Marić <maric.nina@outlook.com'git config --global user.name 'NinaaMaric>
2021-04-04 12:07:10 +0200

    fixed GET_TOTALS in redux devTools

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -8,7 +8,7 @@ const CartContainer = ({ cart = [], total, dispatch }) => {
     dispatch({
       type: GET_TOTALS
     })
-  });
+  }, [cart, dispatch]);
 
   if (cart.length === 0) {
     return (
